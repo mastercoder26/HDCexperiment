f3561ddd4e49625a067ae9d5e30d8d0011178851
Akhil Konduru <akhilkonduru23@gmail.com>
2026-08-25 20:48:33 -0500

    feat: wire named cost profiles into the CLI

--- hdc/__init__.py
+++ hdc/__init__.py
@@ -1,10 +1,14 @@
 """The three names most users need from this small HDC project."""
 
 from .core import DEFAULT_DIMENSIONS, HDC
+from .costs import CostProfile, available_cost_profiles, get_cost_profile
 from .model import HDCClassifier
 
 __all__ = [
     "DEFAULT_DIMENSIONS",
+    "CostProfile",
     "HDC",
     "HDCClassifier",
+    "available_cost_profiles",
+    "get_cost_profile",
 ]
